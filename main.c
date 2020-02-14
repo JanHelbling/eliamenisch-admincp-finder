cfda414ecda8236dfe1d911361ddbd784eb6baa4
Jan Helbling <jh@jan-helbling.ch>
2020-02-14 22:54:32 +0100

    formated sourcecode

--- main.c
+++ main.c
@@ -25,14 +25,14 @@ int main(int argc,char *argv[]){
                 break;
 			default:
 			case '?':
-				fprintf(stderr,"Invalid Option!\n");
-				return 1;
+		fprintf(stderr,"Invalid Option!\n");
+		return 1;
 		}
 	}
-	
+
 	argc -= optind;
 	argv += optind;
-	
+
 	if(!strstr(url,"http")){
 		fprintf(stderr,"You must provide exact one URL with -u!\n");
 		return 1;
@@ -40,36 +40,36 @@ int main(int argc,char *argv[]){
 	int cnt = 0, code;
 	char *buf = malloc(1024);
 	char newurl[512];
-    
-    pid_t pids[threadcount];
-    int pc;
-    
+
+	pid_t pids[threadcount];
+	int pc;
+
 	while(paths[cnt] != NULL){
-        for(pc = 0;pc != threadcount;pc++){
-	    if(paths[cnt] != NULL){
-            sprintf(newurl,"%s%s",url,paths[cnt++]);
-            pids[pc] = fork();
-            if(!pids[pc]){
-		        code = http_func(newurl,buf,12,HEAD,NULL);
-		        if(code == 200){
-			        printf("!!! [Found] !!! -> %s\n",newurl);
-                    int c;
-                    for(c=0;c!=threadcount;c++){
-                        kill(pids[c],SIGTERM);
-                    }
-		            exit(0);
-		        } else {
-			        printf("[CHECKED] -> %s\n",newurl);
-	        	}
-                exit(0);
-	        }
-             }
-            
-        }
-        for(pc=0;pc != threadcount;pc++){
-              wait(NULL);
-        }
-    }
+		for(pc = 0;pc != threadcount;pc++){
+			if(paths[cnt] != NULL){
+				sprintf(newurl,"%s%s",url,paths[cnt++]);
+				pids[pc] = fork();
+				if(!pids[pc]){
+					code = http_func(newurl,buf,12,HEAD,NULL);
+					if(code == 200){
+						printf("!!! [Found] !!! -> %s\n",newurl);
+						int c;
+						for(c=0;c!=threadcount;c++){
+							kill(pids[c],SIGTERM);
+						}
+						exit(0);
+					} else {
+						printf("[CHECKED] -> %s\n",newurl);
+					}
+					exit(0);
+				}
+			}
+
+		}
+		for(pc=0;pc != threadcount;pc++){
+			wait(NULL);
+		}
+	}
 	free(buf);
 	return 0;
 }
